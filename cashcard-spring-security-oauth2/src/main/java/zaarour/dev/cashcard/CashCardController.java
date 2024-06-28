2d37fe8eaae0852f5b7729eaaf01b34f8cbc4f33
jawad-zaarour <zaarourjawade@gmail.com>
2024-06-28 11:28:34 +0300

    accessing-authentication-mvc

--- cashcard-spring-security-oauth2/src/main/java/zaarour/dev/cashcard/CashCardController.java
+++ cashcard-spring-security-oauth2/src/main/java/zaarour/dev/cashcard/CashCardController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.security.core.Authentication;
 
 /**
  * The cash card REST API
@@ -34,8 +35,10 @@ public class CashCardController {
     }
 
     @PostMapping
-    private ResponseEntity<CashCard> createCashCard(@RequestBody CashCard newCashCardRequest, UriComponentsBuilder ucb) {
-        CashCard savedCashCard = cashCards.save(newCashCardRequest);
+    private ResponseEntity<CashCard> createCashCard(@RequestBody CashCardRequest cashCardRequest, UriComponentsBuilder ucb
+            , @CurrentOwner String owner) {
+        CashCard cashCard = new CashCard(cashCardRequest.amount(), owner);
+        CashCard savedCashCard = this.cashCards.save(cashCard);
         URI locationOfNewCashCard = ucb
                 .path("cashcards/{id}")
                 .buildAndExpand(savedCashCard.id())
@@ -44,7 +47,8 @@ public class CashCardController {
     }
 
     @GetMapping
-    public ResponseEntity<Iterable<CashCard>> findAll() {
-        return ResponseEntity.ok(this.cashCards.findAll());
+    public ResponseEntity<Iterable<CashCard>> findAll(@CurrentOwner String owner) {
+        var result = this.cashCards.findByOwner(owner);
+        return ResponseEntity.ok(result);
     }
 }
